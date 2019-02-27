db53e4016e12e767f99909d7bde8185dcc1ccec6
Pranshul Saxena <epicpranshul@gmail.com>
2019-02-27 17:00:20 +0530

    some

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date(Date.parse(new Date()) + 05 * 12 * 60 * 60 * 1000);
 initializeClock('clockdiv', deadline);
 
 
